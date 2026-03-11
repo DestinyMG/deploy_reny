6a44c5f0031b41cedad3963dccc187587704e81f
DestinyMG <DGB0609@gmail.com>
2026-03-11 10:18:23 -0400

    superusuario

--- backend/build.sh
+++ backend/build.sh
@@ -11,26 +11,26 @@ python manage.py collectstatic --no-input
 echo "--- Aplicando migraciones ---"
 python manage.py migrate
 
-echo "--- Verificando Superusuario ---"
-# Este bloque de Python crea el admin solo si no existe
-python manage.py shell <<EOF
-import os
-from django.contrib.auth import get_user_model
+# echo "--- Verificando Superusuario ---"
+# # Este bloque de Python crea el admin solo si no existe
+# python manage.py shell <<EOF
+# import os
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
-# Estos valores los tomará de las variables de entorno de Render
-# Si no las pones, usará los valores por defecto aquí escritos
-username = os.environ.get('ADMIN_USERNAME', 'admin')
-email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
-password = os.environ.get('ADMIN_PASSWORD', 'admin1234')
+# # Estos valores los tomará de las variables de entorno de Render
+# # Si no las pones, usará los valores por defecto aquí escritos
+# username = os.environ.get('ADMIN_USERNAME', 'admin')
+# email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
+# password = os.environ.get('ADMIN_PASSWORD', 'admin1234')
 
-if not User.objects.filter(email=email).exists():
-    print(f"Creando superusuario para {email}...")
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuario creado con éxito.")
-else:
-    print("El superusuario ya existe, saltando paso.")
-EOF
+# if not User.objects.filter(email=email).exists():
+#     print(f"Creando superusuario para {email}...")
+#     User.objects.create_superuser(username=username, email=email, password=password)
+#     print("Superusuario creado con éxito.")
+# else:
+#     print("El superusuario ya existe, saltando paso.")
+# EOF
 
-echo "--- Build finalizado con éxito ---"
+# echo "--- Build finalizado con éxito ---"
